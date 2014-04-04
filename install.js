c4f15045c750d4ee7378e37792c18513289d1386
greg <greg@root-2.com>
2014-04-04 14:46:40 -0700

    Initial

--- install.js
+++ install.js
@@ -9,7 +9,7 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
-
+var i = 0;
 async.waterfall ([
 
     function (done){
@@ -17,11 +17,10 @@ async.waterfall ([
         console.log ("\n\nWELCOME TO THE NODE-AWS-DEPLOY INSTALLER");
         console.log ("This installs your application onto AWS and allows automatic deployment just by pushing to your remote git repository." +
             " Just answer a few questions and everything will be setup");
-        console.log ("1) Enter your applications full path (example: \MyCoolApplication)");
         done ();
     },
     function (done){
-        rl.question("1) Enter your application's folder. (Path only relative to the current directory, no file names. Example  'MyCoolApplication'):", function(answer) {
+        rl.question(++i + ") Enter your application's folder. (Path only relative to the current directory, no file names. Example  'MyCoolApplication'):", function(answer) {
             fs.mkdirSync (answer, function (err){
                 app_path = anwer;
                 rl.close();
@@ -30,14 +29,14 @@ async.waterfall ([
         });
     },
     function (done){
-        rl.question("2) Enter your application's name:", function(answer) {
+        rl.question(++i + ") Enter your application's name:", function(answer) {
             app_name = answer;
             rl.close();
             done ();
         });
     },
     function (done){
-        rl.question("3) Enter your application's entry point (defaults to 'start.js'):", function(answer) {
+        rl.question(++i + ") Enter your application's entry point (defaults to 'start.js'):", function(answer) {
             app_entry = answer;
             rl.close();
             done ();
@@ -59,14 +58,14 @@ async.waterfall ([
         done ();
     },
     function (done){
-        rl.question("4) Please take a second to copy the ssh key from this server, listed below, and paste into your git repository service (such as github). This will authorize this server AMI to " +
+        rl.question(++i + ") Please take a second to copy the ssh key from this server, listed below, and paste into your git repository service (such as github). This will authorize this server AMI to " +
             "clone your repository and perform git pulls. Press <enter> when done" + ssh_file, function(answer) {
             rl.close();
             done ();
         });
     },
     function (done){
-        rl.question("5) Now that your Git repository has been configured, enter the ssh url for remote access so this server can clone it:" , function(answer) {
+        rl.question(++i + ") Now that your Git repository has been configured, enter the ssh url for remote access so this server can clone it:" , function(answer) {
             git_url = answer;
             if (git_url){
                 console.log ("  Cloning your repository...");
@@ -87,7 +86,7 @@ async.waterfall ([
                 done ();
             }
         });
-    },
+    }
 ], function (err){
     if (!err){
         console.log ("Success");
